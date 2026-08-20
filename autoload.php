b1858c0807497326eafb7813a5bf819bb649254b
Aleksandar Zivanovic <acas_mail@yahoo.com>
2026-08-20 23:54:21 +0200

    fix(core): remove redundant include check in autoload.php

--- autoload.php
+++ autoload.php
@@ -1,10 +1,7 @@
 <?php
 
 $configPath = __DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR .  'csrf_config.php';
-
-if (!in_array($configPath, get_included_files())) {
-    require_once $configPath;
-}
+require_once $configPath;
 
 spl_autoload_register(function ($class) {
     $classDir = __DIR__ . DIRECTORY_SEPARATOR . 'src';
